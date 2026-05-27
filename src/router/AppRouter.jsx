ecb361a0217f238b89b026e6fae6a565f11ddc3f
Lia <liafernandez2@gmail.com>
2026-05-27 21:34:39 +0200

    clean up: remove placeholder pages not yet implemented

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,6 @@ import { ROUTES } from "../config/routesConfig";
 import Navbar from "../components/layout/Navbar";
 import WelcomePage from "../pages/WelcomePage/WelcomePage";
 import ExplorePage from "../pages/ExplorePage/ExplorePage";
-import MovieDetailPage from "../pages/MovieDetailPage/MovieDetailPage";
-import ActorDetailPage from "../pages/ActorDetailPage/ActorDetailPage";
-import DirectorDetailPage from "../pages/DirectorDetailPage/DirectorDetailPage";
-import FavoritesPage from "../pages/FavoritesPage/FavoritesPage";
-import ProfilePage from "../pages/ProfilePage/ProfilePage";
-import LoginPage from "../pages/LoginPage/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 export default function AppRouter() {
@@ -17,13 +11,7 @@ export default function AppRouter() {
       <Navbar />
       <Routes>
         <Route path={ROUTES.HOME} element={<WelcomePage />} />
-        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
         <Route path={ROUTES.EXPLORE} element={<ExplorePage />} />
-        <Route path={ROUTES.MOVIE_DETAIL} element={<MovieDetailPage />} />
-        <Route path={ROUTES.ACTOR_DETAIL} element={<ActorDetailPage />} />
-        <Route path={ROUTES.DIRECTOR_DETAIL} element={<DirectorDetailPage />} />
-        <Route path={ROUTES.FAVORITES} element={<FavoritesPage />} />
-        <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
         <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
